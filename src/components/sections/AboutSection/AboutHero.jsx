fd36e51bc6f21a36eae46a7d7d5b56a967058d47
pittu777 <pittuprasanth98@gmail.com>
2025-06-02 15:55:17 +0530

    completed

--- src/components/sections/AboutSection/AboutHero.jsx
+++ src/components/sections/AboutSection/AboutHero.jsx
@@ -5,7 +5,7 @@ const AboutHero = () => {
     <section className='bg-white w-full px-0 md:px-0 py-15'>
         <div className='max-w-4xl mx-auto text-center'>
             <p className='text-[rgba(127,86,217,1)] font-semibold mb-2'>About us</p>
-            <h1 className='text-[34px leading-[44px] sm:text-gray-900
+            <h1 className='text-[34px] leading-[44px] sm:text-gray-900
             md:text-6xl font-semibold text-gray-900 mb-6 xl:text-[48px] 
             xl:tracking-[-0.02em]'>About the Company <br className='hidden md:block'/></h1>
             <p className='
